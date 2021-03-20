6ef0572a209c845520e7fd36fa76e02b14cc4a5d
Maciej Pucek <70510311+Maciej1994@users.noreply.github.com>
2021-03-20 18:52:00 +0100

    Add files via upload

---
+++ PRZEDSZK - Przedszkolanka/PRZEDSZK - Przedszkolanka.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int a, b, c, d;
+
+	cout << " podaj ilosc testow: ";
+	int ile;
+	cin >> ile;
+
+
+	for (int j{ 0 }; j < ile; j++)
+	{
+
+
+		cout << " podaj 1 dzielnik: ";
+		cin >> a;
+		cout << " podaj 2 dzielnik: ";
+		cin >> b;
+
+
+		for (int i{ 1 }; ; ++i)
+		{
+
+			c = i % a;
+			d = i % b;
+
+			if (c == 0 && d == 0)
+			{
+				cout << i;
+				cout << endl;
+				break;
+			}
+		}
+
+
+	}
+	
+}
+
