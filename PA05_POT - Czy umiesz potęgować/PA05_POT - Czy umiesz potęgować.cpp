42d9092e36c8bed40a1af9d900b2726ebbc5d566
Maciej Pucek <70510311+Maciej1994@users.noreply.github.com>
2021-03-20 18:51:28 +0100

    Add files via upload

---
+++ PA05_POT - Czy umiesz potęgować/PA05_POT - Czy umiesz potęgować.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int ile_liczb()
+{
+    cout << " podaj ile liczb: ";
+    int ile;
+    cin >> ile;
+    return ile;
+}
+
+int dzialanie()
+{
+    cout << "Podaj podstawe i wykladnik ";
+    int a, b;
+    cin >> a >> b;
+    int suma = pow(a, b);
+    return suma;
+}
+
+void pokaz_jednosci(int& sum)
+{
+    cout << (sum % 10);
+    cout << endl;
+}
+
+int main()
+{
+    int ile = ile_liczb();
+    for (int i{ 0 }; i < ile; ++i)
+    {
+        int suma = dzialanie();
+        pokaz_jednosci(suma);
+    }
+}
+
