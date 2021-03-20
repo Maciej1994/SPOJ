970c09dd89d32abcd0dd1ff6c683736f32fcb0d7
Maciej Pucek <70510311+Maciej1994@users.noreply.github.com>
2021-03-20 18:52:20 +0100

    Add files via upload

---
+++ TABLICA - Tablica/TABLICA - Tablica.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include<array>
+using namespace std;
+
+int main()
+{
+    
+    array<int, 5>arr1;
+   
+
+    cout << " podaj kilka liczb ( max 5 ):\n";
+
+    for (int i{ 0 }; i < arr1.size(); ++i)
+    {
+        cout << " liczba: " << i + 1 << " : ";
+        cin >> arr1[i];
+    }
+
+    for (int j{ 4 }; j >= 0; j--)
+    {
+        cout << arr1[j];
+    }
+}
+
+
